067def5b629a3479893a829dd015cfcda2fdc6de
felipemm-santos <tfelipesantos99s@gmail.com>
2026-01-02 15:18:03 -0300

    feat: Add members page

--- tech/urls.py
+++ tech/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from tech.views import tech_home
+from tech.views import members_area, tech_home
 
 urlpatterns = [
     path('', tech_home, name='tech_home'),
+    path('members/', members_area, name='members_area'),
 ]
--- tech/views.py
+++ tech/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def tech_home(request):
     return render(request, 'index.html')
+
+@login_required
+def members_area(request):
+    return render(request, 'members.html')
